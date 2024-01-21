2a9b2ed94b2e06cd8715dfa3d5949bf43d6d71ad
Ben Shelley <shellbend@gmail.com>
2024-01-20 17:16:56 -0700

    Add initial api and stats modules.

---
+++ src/yohonhl/api.py
@@ -0,0 +1,48 @@
+"""NHL API functions."""
+
+from datetime import datetime
+from enum import Enum
+
+import requests
+
+URL = "https://api-web.nhle.com/v1"
+
+
+class DateStr(Enum):
+    """Date strings used in API requests."""
+
+    WEEK = "week"
+    MONTH = "month"
+    NOW = "now"
+
+
+def get_team_schedule(
+    team_abbrev: str, datestr: DateStr = DateStr.NOW, timeout: float = 5
+) -> dict:
+    """Get the schedule for a single team."""
+    if datestr == DateStr.NOW:
+        url = f"{URL}/club-schedule-season/{team_abbrev}/now"
+    else:
+        url = f"{URL}/club-schedule/{team_abbrev}/{datestr.value}"
+    return requests.get(url, timeout=timeout).json()
+
+
+def get_schedule(datestr: str = DateStr.NOW.value, timeout: float = 5) -> dict:
+    """Get the schedule for all teams."""
+    if isinstance(datestr, str) and datestr != "now":
+        fmt = "%Y-%m-%d"
+        datestr = datetime.strftime(
+            datetime.strptime(datestr, fmt),  # noqa: DTZ007
+            fmt,
+        )
+    return requests.get(f"{URL}/schedule/{datestr}", timeout=timeout).json()
+
+
+def get_linescores(timeout: float = 5) -> dict:
+    """Get linescore details."""
+    return requests.get(f"{URL}/score/now", timeout=timeout).json()
+
+
+def get_boxscores(game_id: int, timeout: float = 5) -> dict:
+    """Get the boxscore for a game."""
+    return requests.get(f"{URL}/gamecenter/{game_id}/boxscore", timeout=timeout).json()
---
+++ src/yohonhl/stats.py
@@ -0,0 +1,70 @@
+"""Stats aggregation and calculations."""
+
+from dataclasses import dataclass
+from datetime import date
+from datetime import datetime
+from datetime import time
+
+from yohonhl import api
+
+
+@dataclass
+class Goal:
+    """Goal dataclass."""
+
+    season: int
+    game_id: int
+    game_date: date
+    period: int
+    time_in_period: time
+    player_name: str
+    player_team: str
+    home_team: str
+    away_team: str
+    home_score: int
+    away_score: int
+    strength: str
+
+
+def goals_from_linescores(linescores: dict) -> list[Goal]:
+    """Get a list of goals from linescore data."""
+    all_goals = []
+    for game in linescores["games"]:
+        if game["gameState"] != "OFF":
+            continue
+        game_id = int(game["id"])
+        season = int(game["season"])
+        game_date = datetime.strptime(game["gameDate"], "%Y-%m-%d").date()  # noqa: DTZ007
+        away_team = game["awayTeam"]["abbrev"]
+        home_team = game["homeTeam"]["abbrev"]
+        try:
+            game_goals = game["goals"]
+        except KeyError:
+            # Some games might not have any goals yet, e.g. in-progress games
+            continue
+        for goal in game_goals:
+            all_goals.append(
+                Goal(
+                    season=season,
+                    game_id=game_id,
+                    game_date=game_date,
+                    period=int(goal["period"]),
+                    time_in_period=datetime.strptime(  # noqa: DTZ007
+                        goal["timeInPeriod"], "%M:%S"
+                    ).time(),
+                    player_name=goal["name"]["default"],
+                    player_team=goal["teamAbbrev"],
+                    home_team=home_team,
+                    away_team=away_team,
+                    home_score=int(goal["homeScore"]),
+                    away_score=int(goal["awayScore"]),
+                    strength=goal["strength"],
+                )
+            )
+    return all_goals
+
+
+def get_goals() -> list[Goal]:
+    """Get a list of all goals from current linescores."""
+    ls = api.get_linescores()
+    return goals_from_linescores(ls)
