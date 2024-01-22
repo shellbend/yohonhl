e7cf8ded3aaf2195d0385a4bebc6802de77e0e48
Ben Shelley <shellbend@gmail.com>
2024-01-21 19:11:09 -0700

    Add goals command.

--- src/yohonhl/__main__.py
+++ src/yohonhl/__main__.py
@@ -1,11 +1,37 @@
 """Command-line interface."""
 import click
+import pandas as pd
+
+from yohonhl import stats
 
 
-@click.command()
+@click.group()
 @click.version_option()
 def main() -> None:
     """YohoNHL."""
+
+
+@main.command()
+@click.option(
+    "-o",
+    "--output",
+    help="Write goals to output file as CSV",
+    type=click.Path(dir_okay=False, writable=True, allow_dash=True),
+    default="-",
+)
+@click.option(
+    "-a",
+    "--append",
+    help="Append to the output file if it exists.",
+    is_flag=True,
+    default=False,
+)
+def goals(output: str, append: bool) -> None:
+    """Get goal data for this weeks' games."""
+    g = stats.get_goals()
+    df = pd.DataFrame(g)
+    with click.open_file(output, mode="a" if append else "w") as f:
+        df.to_csv(f, index=False, header=not append)
 
 
 if __name__ == "__main__":
