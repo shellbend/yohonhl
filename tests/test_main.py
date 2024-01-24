99b091577510eb7dba0c943f5dbb82a6dddc0bd9
Ben Shelley <shellbend@gmail.com>
2024-01-23 21:54:36 -0700

    Test goals subcommand.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,12 @@
 """Test cases for the __main__ module."""
+
+from typing import Any
+
 import pytest
 from click.testing import CliRunner
 
 from yohonhl import __main__
+from yohonhl import api
 
 
 @pytest.fixture()
@@ -15,3 +19,14 @@ def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(__main__.main)
     assert result.exit_code == 0
+
+
+def test_goals_succeeds(
+    runner: CliRunner, linescore_data: Any, requests_mock: Any
+) -> None:
+    """Goals subcommand succeeds."""
+    resp_data, expected_goals = linescore_data
+    requests_mock.get(f"{api.URL}/score/now", json=resp_data)
+    result = runner.invoke(__main__.main, ["goals"])
+    assert result.exit_code == 0
+    assert result.output
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,113 +1,16 @@
 """Unit tests for yohonhl.stats module."""
 
 
-import json
-from datetime import date
-from datetime import time
 from typing import Any
-
-import pytest
 
 from yohonhl import api
 from yohonhl import stats
 
 
-@pytest.fixture()
-def linescore_data() -> tuple[dict[Any, Any], list[stats.Goal]]:
-    """Fixture for linescore and corresponding goal data."""
-    goals = [
-        stats.Goal(
-            season=20232024,
-            game_id=2023020718,
-            game_date=date(2024, 1, 21),
-            period=1,
-            time_in_period=time(0, 11, 57),
-            player_name="E. Zamula",
-            player_team="PHI",
-            home_team="PHI",
-            away_team="OTT",
-            home_score=1,
-            away_score=0,
-            strength="PP",
-        ),
-        stats.Goal(
-            season=20232024,
-            game_id=2023020718,
-            game_date=date(2024, 1, 21),
-            period=1,
-            time_in_period=time(0, 16, 40),
-            player_name="E. Zamula",
-            player_team="PHI",
-            home_team="PHI",
-            away_team="OTT",
-            home_score=2,
-            away_score=0,
-            strength="EV",
-        ),
-    ]
-    resp_data = {
-        "games": [
-            {
-                "id": 2023020718,
-                "season": 20232024,
-                "gameDate": goals[0].game_date.strftime("%Y-%m-%d"),
-                "gameState": "OFF",
-                "awayTeam": {
-                    "abbrev": "OTT",
-                },
-                "homeTeam": {
-                    "abbrev": "PHI",
-                },
-                "goals": [
-                    {
-                        "period": g.period,
-                        "timeInPeriod": g.time_in_period.strftime("%M:%S"),
-                        "name": {
-                            "default": g.player_name,
-                        },
-                        "teamAbbrev": g.player_team,
-                        "awayScore": g.away_score,
-                        "homeScore": g.home_score,
-                        "strength": g.strength,
-                    }
-                    for g in goals
-                ],
-            },
-            {
-                "id": 2023020719,
-                "season": 20232024,
-                "gameDate": "2024-01-21",
-                "gameState": "LIVE",
-                "gameScheduleState": "OK",
-                "awayTeam": {
-                    "abbrev": "MIN",
-                },
-                "homeTeam": {
-                    "abbrev": "CAR",
-                },
-            },
-            {
-                "id": 2023020719,
-                "season": 20232024,
-                "gameDate": "2024-01-21",
-                "gameState": "OFF",
-                "gameScheduleState": "OK",
-                "awayTeam": {
-                    "abbrev": "MIN",
-                },
-                "homeTeam": {
-                    "abbrev": "CAR",
-                },
-            },
-        ],
-    }
-    return resp_data, goals
-
-
 def test_get_goals(linescore_data: Any, requests_mock: Any) -> None:
     """Test getting goals."""
     resp_data, expected_goals = linescore_data
-    requests_mock.get(f"{api.URL}/score/now", text=json.dumps(resp_data))
+    requests_mock.get(f"{api.URL}/score/now", json=resp_data)
     goals = stats.get_goals()
     assert len(goals) == len(expected_goals)
     for g, eg in zip(goals, expected_goals):
